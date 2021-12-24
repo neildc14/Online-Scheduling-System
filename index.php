35aff1424be5cc6469160dc02ee9f06ee8cbd9b7
neil dela cruz <genoznightmare@gmail.com>
2021-12-24 11:09:17 +0800

    finished frontpage

--- index.php
+++ index.php
@@ -10,27 +10,27 @@
         <div class="search-bar">
             <input type="text" placeholder="">
             <button><i class="fa fa-search" aria-hidden="true"></i></button>
-        </div>
-        
+        </div>  
     </div>
     
-    <section>
-        <h2>We hear your pain.<br> We heal your pain!</h2>
-        
+    <section class="aside__section">
+
         <aside class="container-fluid">
-        <div class="aside__container">
-            <div class="aside__container_header">
-                <div></div>
-                <h2>Check App!</h2>
-            </div>
-            
-            <div class="aside__container_button">
-                <button type="button" class="consult-online">Consult Online</button>
-                <button type="button" class="check-appointment">Check Appointment</button>
-            </div>
-            
-        </div>
-    </aside>
+            <div class="aside__container">
+                <div class="aside__container_header">
+                    <div></div>
+                    <h2>Check App!</h2>
+                </div>
+                
+                <div class="aside__container_button">
+                    <button type="button" class="consult-online">Consult Online</button>
+                    <button type="button" class="check-appointment">Set Appointment</button>
+                </div>  
+            </div>  
+        </aside>
+
+        <h2 class="tagline">We <span>hear</span> your pain.<br> We <span>heal</span> your pain!</h2>
+
     </section>
 </main>
 
