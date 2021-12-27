9b6c1439d32ebdf87189de8dd6ccb86c23a6cf38
neil dela cruz <genoznightmare@gmail.com>
2021-12-27 12:40:43 +0800

    mobile-to-desktop-view frontpage stable now

---
+++ templates/hero.php
@@ -0,0 +1,8 @@
+<main class="container-fluid text-center">
+    <div class="hero text-center container">
+        <h1>EPIONE MEDICAL CENTER</h1>
+        <div class="search-bar">
+            <input type="text" placeholder="">
+            <button><i class="fa fa-search" aria-hidden="true"></i></button>
+        </div>  
+    </div>
